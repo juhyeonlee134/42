107aaef6c284c9fa4e0e77acad48a5de8f9e67e9
Juhyeon Lee <juhyelee@c6r1s8.42seoul.kr>
2024-03-20 13:54:29 +0900

    cub3d : feat get next line

--- 09_cub3d/includes/parsing.h
+++ 09_cub3d/includes/parsing.h
@@ -5,63 +5,20 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: juhyelee <juhyelee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/06 21:51:36 by juhyelee          #+#    #+#             */
-/*   Updated: 2023/09/08 20:49:46 by juhyelee         ###   ########.fr       */
+/*   Created: 2024/03/20 12:33:15 by juhyelee          #+#    #+#             */
+/*   Updated: 2024/03/20 13:36:06 by juhyelee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSING_H
 # define PARSING_H
 
-# include "libft.h"
-# include "get_next_line.h"
-# include "utils.h"
-# include <fcntl.h>
+# define BUFFER_SIZE 31
 
-typedef struct s_info
-{
-	char	**map;
-	int		width;
-	int		height;
-	t_pos	player;
-	size_t	e_num;
-	size_t	c_num;
-}t_info;
-
-typedef struct s_num
-{
-	size_t	player;
-	size_t	exit;
-	size_t	coin;
-}t_num;
-
-char	**parse(const char *file);
-
-int		get_fd(const char *file);
-char	**get_map(const int fd, const char *file);
-int		is_valid(const char **map);
-void	remove_map(char **map);
-
-int		get_height_by_file(const char *file);
-void	set_map(const int fd, const size_t height, char **map);
-void	set_line(char *line);
-
-int		is_valid_map(const char **map);
-int		is_valid_root(const char **map);
-
-int		is_rectangle(const char **map);
-int		is_wall_arround(const char **map);
-int		is_all_one(const char *line);
-int		is_contain(const char **map);
-
-void	dup_map(t_info *info, const char **map);
-void	check_element(const char **map, size_t *e_num, size_t *c_num);
-void	clear_map(char **map, const size_t index);
-size_t	get_height_by_map(const char **map);
-
-void	found_player_pos(t_info *info);
-void	check_all_path(t_info *info, const int x, const int y);
-
-int		check_path(t_info *info, const int x, const int y);
+char	*__get_string(int const fd);
+int		__is_not_contain(char const *string, char const delimiter);
+char	*__merge(char *dest, char const *org);
+char	*__extract_string(char const *string, char const delimiter);
+void	__separate_string(char *string, char const delimiter);
 
 #endif
---
+++ 09_cub3d/srcs/get_string.c
@@ -0,0 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_string.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juhyelee <juhyelee@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/20 12:33:52 by juhyelee          #+#    #+#             */
+/*   Updated: 2024/03/20 13:46:16 by juhyelee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "parsing.h"
+
+char	*__get_string(int const fd)
+{
+	int			read_len;
+	char		read_buffer[BUFFER_SIZE + 1];
+	char		*ret;
+	static char	*remain;
+
+	while (__is_not_contain(remain, '\n'))
+	{
+		read_len = read(fd, read_buffer, BUFFER_SIZE);
+		read_buffer[read_len] = '\0';
+		if (read_len <= 0)
+			break ;
+		remain = __merge(remain, read_buffer);
+	}
+	if (!remain || remain[0] == '\0')
+	{
+		free(remain);
+		remain = NULL;
+		return (NULL);
+	}
+	ret = __extract_string(remain, '\n');
+	__separate_string(remain, '\n');
+	return (ret);
+}
+
+int	__is_not_contain(char const *string, char const delimiter)
+{
+	if (!string)
+		return (1);
+	while (*string)
+	{
+		if (*string == delimiter)
+			return (0);
+		string++;
+	}
+	return (1);
+}
+
+char	*__merge(char *dest, char const *org)
+{
+	char			*ret;
+	size_t			dest_len;
+	size_t const	org_len = ft_strlen(org);
+
+	if (!dest)
+	{
+		ret = (char *)malloc(sizeof(char) * (org_len + 1));
+		if (!ret)
+			exit(EXIT_FAILURE);
+		ft_strlcpy(ret, org, org_len + 1);
+		return (ret);
+	}
+	dest_len = ft_strlen(dest);
+	ret = (char *)malloc(sizeof(char) * (dest_len + org_len) + 1);
+	if (!ret)
+		exit(1);
+	ft_strlcpy(ret, dest, dest_len + 1);
+	ft_strlcat(ret, org, dest_len + org_len + 1);
+	return (ret);
+}
+
+char	*__extract_string(char const *string, char const delimiter)
+{
+	char	*ret;
+	size_t	len_to_del;
+
+	len_to_del = 0;
+	while (string[len_to_del] != delimiter)
+		len_to_del++;
+	ret = (char *)malloc(sizeof(char) * (len_to_del + 1));
+	if (!ret)
+		exit(EXIT_FAILURE);
+	ft_strlcpy(ret, string, len_to_del + 1);
+	return (ret);
+}
+
+void	__separate_string(char *string, char const delimiter)
+{
+	size_t	len_to_del;
+	size_t	index;
+
+	len_to_del = 0;
+	while (string[len_to_del] != delimiter)
+		len_to_del++;
+	index = 0;
+	while (string[len_to_del + index + 1])
+	{
+		string[index] = string[len_to_del + index + 1];
+		index++;
+	}
+	string[index] = '\0';
+}
