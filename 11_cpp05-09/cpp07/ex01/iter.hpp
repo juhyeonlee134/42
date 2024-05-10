db5ffa9df0419eeeff75c116a439c32793f171a6
juhyelee <griiim134@gmail.com>
2024-05-10 14:28:14 +0900

    feat : cpp05 ex01

--- 11_cpp05-09/cpp07/ex01/iter.hpp
+++ 11_cpp05-09/cpp07/ex01/iter.hpp
@@ -2,13 +2,44 @@
 #ifndef __ITER_H__
 #define __ITER_H__
 
-template <typename T>
-void iter(T * arr, int len, void (*func)(T &))
+#include <iostream>
+
+template <typename T, typename F>
+void iter(T const * const arr, std::size_t const len, void (*func)(F const &))
 {
-	for (int i = 0; i < len; i++)
+	if (arr == NULL || func == NULL)
 	{
-		func(arr[i]);
+		return ;
 	}
+	for (std::size_t index = 0; index < len; index++)
+	{
+		func(arr[index]);
+	}
+}
+
+template<typename T, typename F>
+void iter(T * const arr, std::size_t const len, void (*func)(F &))
+{
+	if (arr == NULL || func == NULL)
+	{
+		return ;
+	}
+	for (std::size_t index = 0; index < len; index++)
+	{
+		func(arr[index]);
+	}
+}
+
+template<typename T>
+void print(T const & t)
+{
+	std::cout << t << '\n';
+}
+
+template<typename T>
+void addTwo(T & t)
+{
+	t += 2;
 }
 
 #endif
