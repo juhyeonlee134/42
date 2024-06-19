f1c89fcce64563b6512047126c6977fc3a256968
juhyeonlee <griiim134@gmail.com>
2024-06-19 10:19:17 +0900

    fix : makefile

--- 12_inception/srcs/requirements/nginx/tools/run.sh
+++ 12_inception/srcs/requirements/nginx/tools/run.sh
@@ -3,6 +3,6 @@
 mkdir /etc/nginx/ssl;
 
 # create ssk key
-openssl req -x509 -newkey rsa:4096 -keyout $TLS_KEY_PATH -out $TLS_CERT_PATH -days 365 -nodes -subj "/C=KR/ST=Seoul/L=Seoul/O=42seoul/OU=inception/CN=juhyelee@42.fr"
+openssl req -x509 -newkey rsa:4096 -keyout $TLS_KEY_PATH -out $TLS_CERT_PATH -days 365 -nodes -subj "/C=KR/ST=Seoul/L=Seoul/O=42seoul/OU=inception/CN=juhyelee@42.fr";
 
 nginx -g "daemon off;"
